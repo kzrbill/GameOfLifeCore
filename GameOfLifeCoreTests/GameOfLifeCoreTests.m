e8559075fd4b0fcae8c773474274d399d9c83020
Will Grange <will.grange@gmail.com>
2014-08-11 20:45:56 +0100

    Initial Commit

---
+++ GameOfLifeCoreTests/GameOfLifeCoreTests.m
@@ -0,0 +1,34 @@
+//
+//  GameOfLifeCoreTests.m
+//  GameOfLifeCoreTests
+//
+//  Created by Will Grange on 11/08/2014.
+//  Copyright (c) 2014 Will Grange. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GameOfLifeCoreTests : XCTestCase
+
+@end
+
+@implementation GameOfLifeCoreTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
